b540cdfddcd32a727894f5e50ef62225adb94b9b
olimpia marin <olimpiamarin84@gmail.com>
2016-04-09 11:19:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,105 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+/**
+ * Created by icondor on 26/03/16.
+ */
+public class Main {
+
+    /* utility methods for INPUT/OUTPUT using a GUI or Console, please leave them as they are */
+
+    // GUI
+    public static int readIntGUI(String label) {
+        String input = JOptionPane.showInputDialog(null,
+                label);
+        int returnValue = Integer.parseInt(input);
+        return returnValue;
+    }
+
+    public static double readDoubleGUI(String label) {
+        String input = JOptionPane.showInputDialog(null,
+                label);
+        double returnValue = Double.parseDouble(input);
+        return returnValue;
+    }
+
+    public static String readStringGUI(String label) {
+        String input = JOptionPane.showInputDialog(null,
+                label);
+        return input;
+    }
+
+    public static void printGUI(String text) {
+        JOptionPane.showMessageDialog(null, text);
+    }
+
+    public static void printGUI(int text) {
+        JOptionPane.showMessageDialog(null, text);
+    }
+
+    public static void printGUI(double text) {
+        JOptionPane.showMessageDialog(null, text);
+    }
+
+    // CONSOLE
+    public static String readStringConsole(String label) {
+        System.out.print(label);
+        String input = new Scanner(System.in).nextLine();
+        return input;
+    }
+
+    public static int readIntConsole(String label) {
+        System.out.print(label);
+        int input = new Scanner(System.in).nextInt();
+        return input;
+    }
+
+    public static double readDoubleConsole(String label) {
+        System.out.print(label);
+        double input = new Scanner(System.in).nextDouble();
+        return input;
+    }
+
+    public static void printConsole(String text) {
+        System.out.println(text);
+    }
+
+    public static void printConsole(int text) {
+        System.out.println(text);
+    }
+
+    public static void printConsole(double text) {
+        System.out.println(text);
+    }
+    /* end of utility methods*/
+
+
+    /* here starts the main class - exercitiu in clasa */
+    public static void main(String[] arguments) {
+
+        // citire numar 1
+        //citire numar 2
+        //apel maxim
+        //afisare maxim
+
+        int calin = readIntConsole("nr 1: ");
+        int grigore = readIntConsole("nr 2 ");
+
+        int cucuBauMax = maximMet(calin, grigore);
+        printConsole("maximul este: " + cucuBauMax);
+    }
+
+    public static int maximMet (int n1, int n2) {
+        int m = n1;
+        if (n1 < n2) ;
+        m = n2;
+        return m;
+    }
+    public static int console
+
+
+        //end of main method
+
+
+    }
+// end of class
